37bc70c8db7e18d21dd874f6b33caa2b4fe12b69
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-12-29 10:51:19 +0300

    desktop layout

--- src/App.js
+++ src/App.js
@@ -12,15 +12,25 @@ function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <img className="logo" src={logo} alt="huddle"></img>
-        <img className="mockup" src={mockup} alt="mockup"></img>
-        <h1 className="heading">Build The Community Your Fans Will Love</h1>
-        <p className="text">
-          Huddle re-imagines the way we build communities. You have a voice. but
-          so does your audience. Create connections with your users as you
-          engage in genuine discussion.
-        </p>
-        <button className="register">Register</button>
+        <div className="logo-wrapper">
+          {" "}
+          <img className="logo" src={logo} alt="huddle"></img>
+        </div>
+        <div className="double-col">
+          <div className="col-1">
+            <img className="mockup" src={mockup} alt="mockup"></img>
+          </div>
+          <div className="col-2">
+            <h1 className="heading">Build The Community Your Fans Will Love</h1>
+            <p className="text">
+              Huddle re-imagines the way we build communities. You have a voice.
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+            <button className="register">Register</button>
+          </div>
+        </div>
+
         <div className="social">
           <FontAwesomeIcon className="icon" icon={faFacebookF} />
           <FontAwesomeIcon className="icon" icon={faTwitter} />
